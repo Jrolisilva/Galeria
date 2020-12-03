641acf9f8863028a8cdfc09720cc82e004c62a1b
Junior Silva <jroliveira.sa@outlook.com>
2020-12-03 15:45:10 -0300

    FirstCommit

---
+++ projeto-galeria/src/index.js
@@ -0,0 +1,10 @@
+//Arquivo SASS
+import './scss/index.scss'
+
+// importar dependencias
+import 'jquery'
+import 'bootstrap'
+
+// arquivos javascript
+import './js/core/includes'
+import './js/plugins/cityButtons'
